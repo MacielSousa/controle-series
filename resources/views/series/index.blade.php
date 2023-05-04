2d72c7e31b50937e3396e1298ccc26a2838dbf18
macielsousa <fmacielsousa@gmail.com>
2023-05-04 14:52:03 -0300

    fix: botão de login e sair

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,13 +1,16 @@
 <x-layout title="Series" :mensagem-sucesso="$mensagemSucesso">
+    @auth
     <a class="btn btn-dark mb-2" href={{route('series.create')}}>Adicionar</a>
+    @endauth
 
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-content-center">
-               <a href="{{ route('seasons.index', $serie->id) }}">
+                @auth <a href="{{ route('seasons.index', $serie->id) }}"> @endauth
                    {{$serie -> nome}}
-               </a>
+                @auth </a> @endauth
 
+                @auth
                 <div class="d-flex justify-content-between align-content-center">
                     <a class="btn btn-primary mx-2" href={{route('series.edit', $serie->id)}}>Editar</a>
 
@@ -19,6 +22,7 @@
                         </button>
                     </form>
                 </div>
+                @endauth
 
             </li>
         @endforeach
