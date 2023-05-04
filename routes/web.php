b1526af4c92958d0e1173c655fa24b8845f364ae
macielsousa <fmacielsousa@gmail.com>
2023-05-04 11:48:39 -0300

    feat: criar conta, logout e login.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\SeasonController;
 use App\Http\Controllers\EpisodesController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,7 +28,11 @@ Route::get('/series/{series}/seasons', [SeasonController::class, 'index'])->name
 
 Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('update');
-
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('/register', [UserController::class, 'create'])->name('users.create');
+Route::post('/register', [UserController::class, 'store'])->name('users.create');
 
 //Route::delete('/series/destroy/{serieId}', [SeriesController::class, 'destroy'])->name('series.destroy');
 //Route::controller(SeriesController::class)->group(function (){
